556db870492a22d190bd84ee4df5fb023837b36b
Arnaud <ngmrnd@unife.it>
2018-10-12 16:53:10 +0200

    Added version and released

--- pack.pl
+++ pack.pl
@@ -2,4 +2,4 @@ name(phil_datasets).
 title('Dataset for machine learning with phil').
 version('1.0').
 author('Arnaud Nguembang Fadja','arnaud.nguembafadja@unife.it ').
-
+download('https://github.com/ArnaudFadja/phil_datasets/releases/*.zip').
